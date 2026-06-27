ff55aec67ecced99ab935292ebc9cf619f137d14
nanjakorewa <nanjakorewa0@gmail.com>
2026-06-27 19:05:26 +0900

    delete

--- _t2.mjs
+++
@@ -1,7 +0,0 @@
-import { processInlineShortcode } from "./src/plugins/remark-hugo-shortcodes/parser.ts";
-import { stateflagsHandler } from "./src/plugins/remark-hugo-shortcodes/shortcodes/stateflags.ts";
-const md = `intro\n\n{{% stateflags US-MT US-WY US-ID %}}\n\nmore`;
-const out = processInlineShortcode(md, "stateflags", (a)=>stateflagsHandler(a,"ja"));
-console.log("contains grid:", out.includes("state-flags-grid"));
-console.log("imgs:", (out.match(/US-[A-Z]{2}\.svg/g)||[]));
-console.log("shortcode consumed:", !out.includes("{{%"));
